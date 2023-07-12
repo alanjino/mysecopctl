4d0c1d8d0868ac3eb78568aedf80f82dcbf34ce1
Alan-Jino <jinolavi33@gmail.com>
2023-07-12 08:35:09 +0530

    modified main.go

--- main.go
+++ main.go
@@ -5,8 +5,18 @@ import (
 	"fmt"
 )
 
+func factorial(n int) int {
+	if n == 0 {
+		return 1
+	}
+	return n * factorial(n-1)
+}
 func main() {
 
 	fmt.Println("HELLO WORLD")
+
+	num := 5
+	result := factorial(num)
+	fmt.Printf("The factorial of %d is %d\n", num, result)
 
 }
